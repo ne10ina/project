d671c5aea840f0bfb1cb110cf1cc33c42c060e95
ne10ina <40335637+ne10ina@users.noreply.github.com>
2018-08-30 13:35:02 +0500

    Добавьте файлы проекта.

---
+++ Sound recorder/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NA = NAudio.Wave;
+using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using CSCore.Win32;
+
+
+namespace Sound_recorder
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        private const string CaptureMode = "Capture";
+        static NA.WaveStream mainOutputStream;
+        NA.WaveChannel32 volumeStream;
+        NA.WaveOutEvent player = new NA.WaveOutEvent();
+        private string fileName = "file.wav";
+        private MMDevice _selectedDevice;
+        private WasapiCapture _soundIn;
+        private IWriteable _writer;
+        private IWaveSource _finalSource;
+
+        public MMDevice SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set
+            {
+                _selectedDevice = value;
+                if (value != null)
+                    button1.Enabled = true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////record("open new Type waveaudio Alias recsound", "", 0, 0);
+            ////record("record recsound", null, 0, 0);
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "WAV (*.wav)|*.wav",
+                Title = "Save",
+                FileName = String.Empty
+            };
+            if (sfd.ShowDialog(this) == DialogResult.OK)
+            {
+                StartCapture(sfd.FileName);
+                button1.Enabled = false;
+                button4.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ////record("save recsound mic.wav", "", 0, 0);
+            ////record("close recsound", "", 0, 0);
+
+            deviceList.Items.Clear();
+
+            using (var deviceEnumerator = new MMDeviceEnumerator())
+            using (var deviceCollection = deviceEnumerator.EnumAudioEndpoints(
+                CaptureMode == "Capture" ? DataFlow.Capture : DataFlow.Render, DeviceState.Active))
+            {
+                foreach (var device in deviceCollection)
+                {
+                    var deviceFormat = WaveFormatFromBlob(device.PropertyStore[
+                        new PropertyKey(new Guid(0xf19f064d, 0x82c, 0x4e27, 0xbc, 0x73, 0x68, 0x82, 0xa1, 0xbb, 0x8e, 0x4c), 0)].BlobValue);
+
+                    var it = device + " " + deviceFormat.Channels.ToString(CultureInfo.InvariantCulture);
+
+                    var item = new ListViewItem(device.FriendlyName) { Tag = device };
+                    item.SubItems.Add(deviceFormat.Channels.ToString(CultureInfo.InvariantCulture));
+
+                    deviceList.Items.Add(item);
+                }
+            }
+        }
+
+        private void StartCapture(string fileName)
+        {
+            if (SelectedDevice == null)
+                return;
+
+            if (CaptureMode == "Capture")
+                _soundIn = new WasapiCapture();
+
+            _soundIn.Device = SelectedDevice;
+            _soundIn.Initialize();
+
+            var soundInSource = new SoundInSource(_soundIn);
+            var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            _finalSource = singleBlockNotificationStream.ToWaveSource();
+            _writer = new WaveWriter(fileName, _finalSource.WaveFormat);
+
+            byte[] buffer = new byte[_finalSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
+            {
+                int read;
+                while ((read = _finalSource.Read(buffer, 0, buffer.Length)) > 0)
+                    _writer.Write(buffer, 0, read);
+            };
+
+            //singleBlockNotificationStream.SingleBlockRead += SingleBlockNotificationStreamOnSingleBlockRead; // visualization
+
+            _soundIn.Start();
+        }
+
+        private static WaveFormat WaveFormatFromBlob(Blob blob)
+        {
+            if (blob.Length == 40)
+                return (WaveFormat)Marshal.PtrToStructure(blob.Data, typeof(WaveFormatExtensible));
+            return (WaveFormat)Marshal.PtrToStructure(blob.Data, typeof(WaveFormat));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+            {
+                fileName = textBox1.Text;
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mainOutputStream = new NA.WaveFileReader(fileName);
+            volumeStream = new NA.WaveChannel32(mainOutputStream);
+            player.Init(volumeStream);
+            player.Play();
+            while (player.PlaybackState == NA.PlaybackState.Playing)
+            {
+
+            }
+            mainOutputStream.Dispose();
+            volumeStream.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StopCapture();
+        }
+
+        private void StopCapture()
+        {
+            if (_soundIn != null)
+            {
+                _soundIn.Stop();
+                _soundIn.Dispose();
+                _soundIn = null;
+                _finalSource.Dispose();
+
+                if (_writer is IDisposable)
+                    ((IDisposable)_writer).Dispose();
+
+                button4.Enabled = false;
+                button1.Enabled = true;
+            }
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (deviceList.SelectedItems.Count > 0)
+            {
+                SelectedDevice = (MMDevice)deviceList.SelectedItems[0].Tag;
+            }
+            else
+            {
+                SelectedDevice = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            OnClosing(e);
+            StopCapture();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
